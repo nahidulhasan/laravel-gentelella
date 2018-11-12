e380c9ac62e39e1d9ad44da98e655fccc94498ad
Nahidul Hasan <nahidul.cse@gmail.com>
2018-11-12 18:06:33 +0600

    Modify docker file and routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
