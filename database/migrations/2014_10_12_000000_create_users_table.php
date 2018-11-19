6fbf68309fcee8f354e3d74679bb0f583b775013
Nahidul Hasan <nahidul.cse@gmail.com>
2018-11-19 14:37:59 +0600

    Minor changed in view page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('google_id');
             $table->string('email')->unique();
             $table->string('password')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('avatar_original')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
